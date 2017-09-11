e094237cf53bf5a3768cb5687aaabeb3551f283d
Bin Yang <bin.yang@windriver.com>
2017-09-11 10:52:56 +0800

    Fix bugs in restcall
    fix bug and update dockerfile
    
    Change-Id: I91579ddc91fa4a0a67360d2e72934f6846e2c2c4
    Issue-Id: MULTICLOUD-58
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- newton/docker/build-image.sh
+++ newton/docker/build-image.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+DIRNAME=`dirname $0`
+DOCKER_BUILD_DIR=`cd $DIRNAME/; pwd`
+echo "DOCKER_BUILD_DIR=${DOCKER_BUILD_DIR}"
+cd ${DOCKER_BUILD_DIR}
 
-IMAGE="multicloud-openstack-newton"
-VERSION="latest"
+BUILD_ARGS="--no-cache"
+ORG="onap"
+VERSION="1.0.0-SNAPSHOT"
+PROJECT="multicloud"
+IMAGE="openstack-newton"
+DOCKER_REPOSITORY="nexus3.onap.org:10003"
+IMAGE_NAME="${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/${IMAGE}"
 
-docker build -t ${IMAGE}:${VERSION} .
+if [ $HTTP_PROXY ]; then
+    BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
+fi
+if [ $HTTPS_PROXY ]; then
+    BUILD_ARGS+=" --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
+fi
+
+function build_image {
+    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
+}
+
+function push_image {
+    docker push ${IMAGE_NAME}:${VERSION}
+    docker push ${IMAGE_NAME}:latest
+}
+
+build_image
+push_image
