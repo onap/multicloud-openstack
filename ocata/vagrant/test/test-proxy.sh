d1ead442bc7cfee74c371ca901e705dc07a34381
Guo Ruijing <ruijing.guo@intel.com>
2017-09-19 02:29:04 -0700

    add proxy test cases
    Change-Id: Icc3595eb960c377479fd8f567cd371f013565778
    Issue-Id: MULTICLOUD-95
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ ocata/vagrant/test/test-proxy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MULTICLOUD_PLUGIN_ENDPOINT=http://172.16.77.40:9004/api/multicloud-ocata/v0/openstack-hudson-dc_RegionOne
+TOKEN=$(curl -v -s -H "Content-Type: application/json" -X POST -d '{ }'  $MULTICLOUD_PLUGIN_ENDPOINT/identity/v3/auth/tokens 2>&1 | grep X-Subject-Token | sed "s/^.*: //")
+curl -v -s  -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -X GET $MULTICLOUD_PLUGIN_ENDPOINT/identity/v2.0/tenants
+curl -v -s  -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -X GET $MULTICLOUD_PLUGIN_ENDPOINT/identity/v3/projects
