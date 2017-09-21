cecc352b211aa4d93751067b063bf9ea90021ca5
Guo Ruijing <ruijing.guo@intel.com>
2017-09-21 03:11:27 -0700

    remove docker build workaroud in function test
    Change-Id: I4167b423319a1b6c66f232951c5084b7f854d577
    Issue-Id: MULTICLOUD-95
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- ocata/vagrant/test/test_multicloud.sh
+++ ocata/vagrant/test/test_multicloud.sh
@@ -7,7 +7,6 @@ sudo apt-get install -y docker.io
 cd /openstack/ocata/docker
 sudo docker build -t multicloud-ocata:latest .
 cd /vagrant/test
-cp /openstack/ocata/run.sh .
 sudo docker build -t multicloud-ocata-test:latest .
 sudo docker network create --subnet=172.16.77.0/24 onap
 sudo docker run -d -t  --name ocata-test --network onap --ip 172.16.77.40 -e MSB_ADDR=172.16.77.40 -e MSB_PORT=9006 multicloud-ocata-test
