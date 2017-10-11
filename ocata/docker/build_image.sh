5b72bd46b34f3d647b9099a32c28d282682deda9
Bin Yang <bin.yang@windriver.com>
2017-10-11 16:07:32 +0800

    Add staging docker images for OpenStack plugins
    Change-Id: Ib64f30ab50002dca67c6835866ffc46fc83d7934
    Issue-Id: MULTICLOUD-110
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- ocata/docker/build_image.sh
+++ ocata/docker/build_image.sh
@@ -7,6 +7,7 @@ cd ${DOCKER_BUILD_DIR}
 BUILD_ARGS="--no-cache"
 ORG="onap"
 VERSION="1.0.0-SNAPSHOT"
+STAGING="1.0.0-STAGING"
 PROJECT="multicloud"
 IMAGE="openstack-ocata"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
@@ -20,12 +21,13 @@ if [ $HTTPS_PROXY ]; then
 fi
 
 function build_image {
-    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest .
+    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:${VERSION} -t ${IMAGE_NAME}:latest -t ${IMAGE_NAME}:${STAGING} .
 }
 
 function push_image {
     docker push ${IMAGE_NAME}:${VERSION}
     docker push ${IMAGE_NAME}:latest
+    docker push ${IMAGE_NAME}:${STAGING}
 }
 
 build_image
