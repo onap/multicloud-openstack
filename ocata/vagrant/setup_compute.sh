dc5dfd5f6170b663fc20eaaea9bbb486e24f13c1
Huang Haibin <haibin.huang@intel.com>
2017-12-12 15:39:17 +0800

    Fix interface name issue in multicloud test
    vagrant box bento/ubuntu-16.04 change interface name,
    which cause regression
    
    Issue-ID: MULTICLOUD-57
    
    Change-Id: I3651113b0025c2433eb97263846a5d0b4119852d
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- ocata/vagrant/setup_compute.sh
+++ ocata/vagrant/setup_compute.sh
@@ -4,7 +4,7 @@ set -ex
 
 cd devstack
 cp /vagrant/compute.conf local.conf
-ip=$(ip a s enp0s8 | grep inet | grep -v inet6 | sed "s/.*inet //" | cut -f1 -d'/')
+ip=$(ip a s eth1 | grep inet | grep -v inet6 | sed "s/.*inet //" | cut -f1 -d'/')
 host=$(hostname)
 sed -i -e "s/HOSTIP/$ip/" -e "s/HOSTNAME/$host/" local.conf
 ./stack.sh
