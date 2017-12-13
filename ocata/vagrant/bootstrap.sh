0b94480468844354958707accc83d73dee716ba0
Huang Haibin <haibin.huang@intel.com>
2017-12-13 13:16:32 +0800

    update function test for multicloud
    1. Fix interface name issue in multicloud test due to
    vagrant box bento/ubuntu-16.04 update
    2. disable openstack compute node in default
    
    Issue-ID: MULTICLOUD-57
    
    Change-Id: If43b2bdcd440149200a413789637e7f8ee51f25b
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- ocata/vagrant/bootstrap.sh
+++ ocata/vagrant/bootstrap.sh
@@ -8,9 +8,9 @@ git clone https://github.com/openstack-dev/devstack
 cd devstack; git checkout stable/ocata
 sudo apt-get install openvswitch-switch -y
 sudo ovs-vsctl add-br br-ex
-ip=$(ip a s enp0s9 | grep inet | grep -v inet6 | sed "s/.*inet//" | cut -f2 -d' ')
-sudo ip address flush enp0s9
-sudo ovs-vsctl add-port br-ex enp0s9
+ip=$(ip a s eth2 | grep inet | grep -v inet6 | sed "s/.*inet//" | cut -f2 -d' ')
+sudo ip address flush eth2
+sudo ovs-vsctl add-port br-ex eth2
 sudo ip a a $ip dev br-ex
 sudo ip link set dev br-ex up
-sudo ip link set dev enp0s9 up
+sudo ip link set dev eth2 up
