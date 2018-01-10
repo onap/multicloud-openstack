56aaa18383e237f483db459f31fb9a771dcf81c3
Victor Morales <victor.morales@intel.com>
2018-01-09 17:44:13 -0800

    Refactor vagrant script files
    The scripts placed into the vagrant folder were using sudo
    instruction which is not necessary. This change pretends to
    reduce the number of lines required to deploy an openstack
    instance.
    
    Change-Id: I2668a8e5655b5e0adbea1e4aa0b30fad98110b7a
    Signed-off-by: Victor Morales <victor.morales@intel.com>
    Issue-ID: MULTICLOUD-83

--- ocata/vagrant/bootstrap.sh
+++ ocata/vagrant/bootstrap.sh
@@ -2,16 +2,23 @@
 
 set -ex
 
-sudo apt-get update -y
-sudo apt-get install git -y
-git clone https://github.com/openstack-dev/devstack
-cd devstack; git checkout stable/ocata
-sudo apt-get install openvswitch-switch -y
-sudo ovs-vsctl add-br br-ex
+if [[ $(whoami) != "root" ]]; then
+    echo "Error: This script must be run as root!"
+    exit 1
+fi
+
+apt-get update
+useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+apt-get install -y git openvswitch-switch
+git clone https://github.com/openstack-dev/devstack /opt/stack/devstack --branch stable/ocata
+chown -R stack:stack /opt/stack/
+
+ovs-vsctl add-br br-ex
 ctl_if=$(ifconfig | grep 192.168.* -B 1 | awk -F " " 'NR==4{print $1}')
 ip=$(ip a s $ctl_if | grep inet | grep -v inet6 | sed "s/.*inet//" | cut -f2 -d' ')
-sudo ip address flush $ctl_if
-sudo ovs-vsctl add-port br-ex $ctl_if
-sudo ip a a $ip dev br-ex
-sudo ip link set dev br-ex up
-sudo ip link set dev $ctl_if up
+ip address flush $ctl_if
+ovs-vsctl add-port br-ex $ctl_if
+ip a a $ip dev br-ex
+ip link set dev br-ex up
+ip link set dev $ctl_if up
--- ocata/vagrant/setup_compute.sh
+++ ocata/vagrant/setup_compute.sh
@@ -2,14 +2,13 @@
 
 set -ex
 
-cd devstack
-cp /vagrant/compute.conf local.conf
+cd /opt/stack/devstack
+cp /vagrant/compute.conf ./local.conf
 data_if=$(ifconfig | grep 192.168.* -B 1 | awk -F " " 'NR==1{print $1}')
 ip=$(ip a s $data_if | grep inet | grep -v inet6 | sed "s/.*inet //" | cut -f1 -d'/')
-host=$(hostname)
-sed -i -e "s/HOSTIP/$ip/" -e "s/HOSTNAME/$host/" local.conf
-./stack.sh
+sed -i "s/HOSTIP/$ip/" local.conf
+su stack -c "./stack.sh"
 
-sudo apt-get update -y
-sudo apt-get install -y putty
-echo y | plink -ssh -l vagrant -pw vagrant 192.168.0.10 "bash /vagrant/setup_cell.sh"
+source openrc admin admin
+nova-manage cell_v2 discover_hosts
+nova-manage cell_v2 map_cell_and_hosts
