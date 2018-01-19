6109c35c5bd944b751a6ff9f27d5a717741c21b9
Huang Haibin <haibin.huang@intel.com>
2018-01-19 19:08:47 +0800

    Change services.py dependency
    newton and windriver depend on newton services.py, we have moved
    services.py to /share/newton_base. then we need change dependency
    from newton to newton_base
    
    Change-Id: I01be148995acc5228ef457e0a166db2b193a6677
    Issue-ID: MULTICLOUD-138
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- windriver/titanium_cloud/proxy/views/services.py
+++ windriver/titanium_cloud/proxy/views/services.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Wind River Systems, Inc.
+# Copyright (c) 2017-2018 Wind River Systems, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ import logging
 from rest_framework import status
 
 from titanium_cloud.pub.config import config
-from newton.proxy.views import services as newton_services
+from newton_base.proxy import services as newton_services
 
 logger = logging.getLogger(__name__)
 
