3a5a65cc1ea963bf0de686721c3bbdf1c942c570
Yun Huang <yun.huang@windriver.com>
2018-03-05 22:35:11 +0800

    Update Newton license header
    Change-Id: I2ee874ab6a4f13e7a5b2fe32f80ba0225d086a9f
    Issue-ID: MULTICLOUD-163
    Signed-off-by: Yun Huang <yun.huang@windriver.com>

--- newton/stop.sh
+++ newton/stop.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# Copyright (c) 2017 Wind River Systems, Inc.
+# Copyright 2017-2018 Wind River Systems, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#       http://www.apache.org/licenses/LICENSE-2.0
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-
-#!/bin/bash
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 ps auxww | grep 'manage.py runserver 0.0.0.0:9003' | awk '{print $2}' | xargs kill -9
 ps auxww | grep 'memcached -d -m 2048 -u root -c 1024 -p 11211 -P /tmp/memcached1.pid' | awk '{print $2}' | xargs kill -9
