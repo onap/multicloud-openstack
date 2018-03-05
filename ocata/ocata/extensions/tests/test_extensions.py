df67b3ad5705fbe5432a3e49d301484afae72fee
Yun Huang <yun.huang@windriver.com>
2018-03-05 21:05:30 +0800

    Update Ocata license header
    Change-Id: I45b6a8b1cbc6571a1c09d375b31e42df70b94cfa
    Issue-ID: MULTICLOUD-163
    Signed-off-by: Yun Huang <yun.huang@windriver.com>

--- ocata/ocata/extensions/tests/test_extensions.py
+++ ocata/ocata/extensions/tests/test_extensions.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Intel Corporation.
+# Copyright 2017-2018 Wind River Systems, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
