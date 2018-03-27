d2df16dc0da500c8308bd9f75aa3b8822811ab55
Yun Huang <yun.huang@windriver.com>
2018-03-27 13:24:18 +0800

    Add event/alert check api for ocata
    Change-Id: I63725749d71182ef6729b3ce50c1a373b85fdbf2
    Issue-ID: MULTICLOUD-204
    Signed-off-by: Yun Huang <yun.huang@windriver.com>

--- ocata/ocata/urls.py
+++ ocata/ocata/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from ocata.registration.views import registration
 from newton_base.openoapi import tenants
 from ocata.resource.views import capacity
+from ocata.resource.views import events
 
 urlpatterns = [
     url(r'^', include('ocata.swagger.urls')),
@@ -36,6 +37,9 @@ urlpatterns = [
     # CapacityCheck
     url(r'^api/multicloud-ocata/v0/(?P<vimid>[0-9a-zA-Z_-]+)/capacity_check/?$',
         capacity.CapacityCheck.as_view()),
+    # events
+    url(r'^api/multicloud-ocata/v0/(?P<vimid>[0-9a-zA-Z_-]+)/events_check/?$',
+        events.EventsCheck.as_view()),
 ]
 
 
