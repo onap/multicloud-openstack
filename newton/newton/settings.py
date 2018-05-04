3c13299e1f76c30dccb5509f0d2b30486f29a3ff
Yun Huang <yun.huang@windriver.com>
2018-05-04 14:38:31 +0800

    Add logging AOP middleware for newton
    Change-Id: Id3a69e23f6b5898630c2737d5bf60e1f5ebf1153
    Issue-ID: MULTICLOUD-220
    Signed-off-by: Yun Huang <yun.huang@windriver.com>

--- newton/newton/settings.py
+++ newton/newton/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'newton.middleware.LogContextMiddleware',
 ]
 
 ROOT_URLCONF = 'newton.urls'
