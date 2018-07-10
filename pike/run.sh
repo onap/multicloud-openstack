120f6631aff07c3ab0dafdb305664d832b5b9c26
Huang Haibin <haibin.huang@intel.com>
2018-07-10 19:22:34 +0800

    Add Openstack Pike plugin framework
    Add Pike framework and one function extension
    
    Change-Id: I045ac1f1a920b509a69d7a72f8e60fb108102839
    Issue-ID: MULTICLOUD-276
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

---
+++ pike/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) 2017-2018 Wind River Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+memcached -d -m 2048 -u root -c 1024 -p 11211 -P /tmp/memcached1.pid
+export PYTHONPATH=lib/share
+uwsgi --http :9006 --module pike.wsgi --master --processes 4
