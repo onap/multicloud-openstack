fb6c49e4131e44e145ab60fce7dc355d69763158
Bin Yang <bin.yang@windriver.com>
2018-09-15 18:25:10 +0000

    Add heatbridge handler
    Embedded the heatbridge handler into GET API
    Add multicloud api helper
    
    Change-Id: I96bd5e555dbcaa08d5ac5ac132c004a690f2e41d
    Issue-ID: MULTICLOUD-264
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- windriver/titanium_cloud/urls.py
+++ windriver/titanium_cloud/urls.py
@@ -90,8 +90,6 @@ urlpatterns = [
         registration.APIv1Registry.as_view()),
     url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/exten',
         include('titanium_cloud.extensions.urlsV1')),
-    url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/',
-        include('titanium_cloud.proxy.urlsV1')),
     url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/tenants/?$',
         tenants.APIv1Tenants.as_view()),
     url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/'
@@ -103,4 +101,8 @@ urlpatterns = [
         vesagent_ctrl.APIv1VesAgentCtrl.as_view()),
     url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/infra_workload/?$',
         infra_workload.APIv1InfraWorkload.as_view()),
+    url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/infra_workload/(?P<requri>[0-9a-zA-Z_-]*)/?$',
+        infra_workload.APIv1InfraWorkload.as_view()),
+    url(r'^api/multicloud-titaniumcloud/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/',
+        include('titanium_cloud.proxy.urlsV1')),
 ]
