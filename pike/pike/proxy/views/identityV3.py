70d085921ab6ff1585c9a983a5716208919c41cb
Huang Haibin <haibin.huang@intel.com>
2018-10-15 14:08:21 +0800

    Upgrade proxy v1 for pike
    Change-Id: I9c65c9cf3851cfbbf064b8955ff6fd6b0a35a875
    Issue-ID: MULTICLOUD-377
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- pike/pike/proxy/views/identityV3.py
+++ pike/pike/proxy/views/identityV3.py
@@ -16,6 +16,7 @@ import logging
 
 from django.conf import settings
 from newton_base.proxy import identityV3 as newton_identityV3
+from common.msapi import extsys
 
 logger = logging.getLogger(__name__)
 
@@ -32,3 +33,52 @@ class TokensV2(newton_identityV3.TokensV2):
     def __init__(self):
         self.proxy_prefix = settings.MULTICLOUD_PREFIX
         self._logger = logger
+
+class APIv1Tokens(Tokens):
+    def __init__(self):
+        super(APIv1Tokens, self).__init__()
+        self.proxy_prefix = settings.MULTICLOUD_API_V1_PREFIX
+        # self._logger = logger
+
+
+    def get(self, request, cloud_owner="", cloud_region_id=""):
+        self._logger.info("cloud_owner,cloud_region_id: %s,%s" % (cloud_owner,cloud_region_id))
+        #self._logger.debug("META> %s" % request.META)
+        #self._logger.debug("data> %s" % request.data)
+
+        vimid = extsys.encode_vim_id(cloud_owner, cloud_region_id)
+        return super(APIv1Tokens,self).get(request, vimid)
+
+
+    def post(self, request, cloud_owner="", cloud_region_id=""):
+        self._logger.info("cloud_owner,cloud_region_id: %s,%s" % (cloud_owner,cloud_region_id))
+        # self._logger.debug("META> %s" % request.META)
+        # self._logger.debug("data> %s" % request.data)
+
+        vimid = extsys.encode_vim_id(cloud_owner, cloud_region_id)
+        return super(APIv1Tokens,self).post(request, vimid)
+
+
+class APIv1TokensV2(TokensV2):
+    def __init__(self):
+        super(APIv1TokensV2, self).__init__()
+        self.proxy_prefix = settings.MULTICLOUD_API_V1_PREFIX
+        # self._logger = logger
+
+
+    def get(self, request, cloud_owner="", cloud_region_id=""):
+        self._logger.info("cloud_owner,cloud_region_id: %s,%s" % (cloud_owner,cloud_region_id))
+        # self._logger.debug("META> %s" % request.META)
+        # self._logger.debug("data> %s" % request.data)
+
+        vimid = extsys.encode_vim_id(cloud_owner, cloud_region_id)
+        return super(APIv1TokensV2,self).get(request, vimid)
+
+
+    def post(self, request, cloud_owner="", cloud_region_id=""):
+        self._logger.info("cloud_owner,cloud_region_id: %s,%s" % (cloud_owner,cloud_region_id))
+        # self._logger.debug("META> %s" % request.META)
+        # self._logger.debug("data> %s" % request.data)
+
+        vimid = extsys.encode_vim_id(cloud_owner, cloud_region_id)
+        return super(APIv1TokensV2,self).post(request, vimid)
