2b7e513a3f2b3cf1b55d18ad4a1e0298b7edfa97
Huang Haibin <haibin.huang@intel.com>
2018-10-15 14:31:14 +0800

    Upgrade tenants v1 for ocata
    Change-Id: I613e079f2355e370206c5c97f34ccb29ed46df0b
    Issue-ID: MULTICLOUD-378
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- ocata/ocata/urls.py
+++ ocata/ocata/urls.py
@@ -54,6 +54,8 @@ urlpatterns = [
         include('ocata.extensions.urlsV1')),
     url(r'^api/multicloud-ocata/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/',
         include('ocata.proxy.urlsV1')),
+    url(r'^api/multicloud-ocata/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/tenants/?$',
+        tenants.APIv1Tenants.as_view()),
     url(r'^api/multicloud-ocata/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/infra_workload/?$',
         infra_workload.APIv1InfraWorkload.as_view()),
     url(r'^api/multicloud-ocata/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/infra_workload/(?P<requri>[0-9a-zA-Z_-]*)/?$',
