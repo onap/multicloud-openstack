2301d7df140c21316c0907f98d03f918d074cf24
Huang Haibin <haibin.huang@intel.com>
2018-10-15 14:28:07 +0800

    Upgrade tenants V1 for pike
    Change-Id: I2fc9cede54534cc885d348e0b86908bcc3dbd49d
    Issue-ID: MULTICLOUD-377
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- pike/pike/urls.py
+++ pike/pike/urls.py
@@ -44,6 +44,8 @@ urlpatterns = [
         include('pike.extensions.urlsV1')),
     url(r'^api/multicloud-pike/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/',
         include('pike.proxy.urlsV1')),
+    url(r'^api/multicloud-pike/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/tenants/?$',
+        tenants.APIv1Tenants.as_view()),
 
     url(r'^api/multicloud-pike/v1/(?P<cloud_owner>[0-9a-zA-Z_-]+)/(?P<cloud_region_id>[0-9a-zA-Z_-]+)/infra_workload/?$',
         infra_workload.APIv1InfraWorkload.as_view()),
