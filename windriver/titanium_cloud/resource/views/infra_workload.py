0f8c7107308a0191c2c4df8a3070ed8868c64480
Xiaohua Zhang <xiaohua.zhang@windriver.com>
2018-11-30 08:52:54 +0000

    Fix issue of Delete Infra_workload
    Enforce client to pass in workload_id
    
    Change-Id: I017321920aad665bee352499e0b43f5c5db5898c
    Issue-ID: MULTICLOUD-423
    Signed-off-by: Xiaohua Zhang <xiaohua.zhang@windriver.com>

--- windriver/titanium_cloud/resource/views/infra_workload.py
+++ windriver/titanium_cloud/resource/views/infra_workload.py
@@ -196,6 +196,12 @@ class InfraWorkload(APIView):
         self._logger.debug("META: %s" % request.META)
 
         try:
+            if requri == "":
+                raise VimDriverNewtonException(
+                    message="workload_id is not specified",
+                    content="workload_id must be specified to delete the workload",
+                    status_code=400)
+
             # assume the workload_type is heat
             stack_id = requri
             cloud_owner, regionid = extsys.decode_vim_id(vimid)
