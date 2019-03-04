9df584ea5d7da99b0ddd8257758948b8c69a26e4
Changjun Wang <310397125@qq.com>
2019-03-04 09:55:21 +0000

    Add Lenovo ThinkCLoud Driver
    add a new branch lenovo
    modify pom.xml under the root
    
    Change-Id: I5edd57bf3e9cc1e32c5a94b9494daf645b220eaa
    Issue-ID: MULTICLOUD-506
    Signed-off-by: Changjun Wang <310397125@qq.com>

---
+++ lenovo/vagrant/test/test-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright (c) 2018 Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+MULTICLOUD_PLUGIN_ENDPOINT=http://172.16.77.40:9010/api/multicloud-thinkcloud/v0/openstack-hudson-dc_RegionOne
+TOKEN=$(curl -v -s -H "Content-Type: application/json" -X POST -d '{ }'  $MULTICLOUD_PLUGIN_ENDPOINT/identity/v3/auth/tokens 2>&1 | grep X-Subject-Token | sed "s/\r//g" | sed "s/^.*: //" |  cat -v)
+#curl -v -s  -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -X GET $MULTICLOUD_PLUGIN_ENDPOINT/image/v2/images
+PROJECT_ID=$(curl -v -s  -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -X GET $MULTICLOUD_PLUGIN_ENDPOINT/identity/v3/projects 2>/dev/null | python -mjson.tool | grep -B5 "name.*\"admin" | grep '\"id\"' | cut -f4 -d'"')
+curl -v -s  -H "Content-Type: application/json" -H "X-Auth-Token: $TOKEN" -X GET $MULTICLOUD_PLUGIN_ENDPOINT/identity/v3/projects/$PROJECT_ID
