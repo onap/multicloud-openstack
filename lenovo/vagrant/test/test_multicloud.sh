e72b9856683f2556a673e7d8785c1af5add46388
Changjun Wang <310397125@qq.com>
2019-03-04 07:34:08 +0000

    Add Lenovo ThinkCLoud Driver
    add a new branch lenovo
    modify pom.xml under the root
    
    Change-Id: I5edd57bf3e9cc1e32c5a94b9494daf645b220eaa
    Issue-ID: MULTICLOUD-506
    Signed-off-by: Changjun Wang <310397125@qq.com>

---
+++ lenovo/vagrant/test/test_multicloud.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Copyright (c) 2018 Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+sudo apt-get update -y
+sudo apt-get install -y docker.io maven npm virtualenv python-dev
+
+git clone http://gerrit.onap.org/r/oparent
+mkdir $HOME/.m2
+cp oparent/settings.xml $HOME/.m2
+
+git clone /openstack
+cd openstack
+mvn clean install
+cp thinkcloud/target/multicloud-openstack-thinkcloud*.zip thinkcloud/vagrant/test/multicloud-openstack-thinkcloud.zip
+
+cd thinkcloud/vagrant/test
+sudo docker build -t multicloud-thinkcloud-test:latest .
+sudo docker network create --subnet=172.16.77.0/24 onap
+sudo docker run -d -t  --name thinkcloud-test --network onap --ip 172.16.77.40 -e MSB_ADDR=172.16.77.40 -e MSB_PORT=9010 multicloud-thinkcloud-test
+
+while true; do
+    sleep 10
+    curl http://172.16.77.40:9010/api/multicloud-thinkcloud/v0/swagger.json && break
+done
+
+for i in `cat tests`
+do
+    bash ./$i
+done
