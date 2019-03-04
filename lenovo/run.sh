3462aa630b246e6da7be1a60d47a63b48356a9a4
Changjun Wang <310397125@qq.com>
2019-03-04 06:27:15 +0000

    Add Lenovo ThinkCLoud Driver
    add a new branch lenovo
    modify pom.xml under the root
    
    Change-Id: I5edd57bf3e9cc1e32c5a94b9494daf645b220eaa
    Issue-ID: MULTICLOUD-506
    Signed-off-by: Changjun Wang <310397125@qq.com>

---
+++ lenovo/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright (c) 2017-2018 Lenovo Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+memcached -d -m 2048 -u root -c 1024 -p 11211 -P /tmp/memcached1.pid
+export PYTHONPATH=lib/share
+
+#service rabbitmq-server restart
+# make sure only 1 worker due to missing the synchronization between workers now
+#nohup celery -A thinkcloud worker --concurrency=1 --loglevel=info &
+
+uwsgi --http :9010 --module thinkcloud.wsgi --master --processes 4
---
+++ lenovo/vagrant/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright (c) 2018 Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+
+sudo apt-get update -y
+sudo apt-get install git -y
+git clone https://github.com/openstack-dev/devstack
+cd devstack; git checkout stable/thinkcloud
+sudo apt-get install openvswitch-switch -y
+sudo ovs-vsctl add-br br-ex
+ctl_if=$(ifconfig | grep 192.168.* -B 1 | awk -F " " 'NR==4{print $1}')
+ip=$(ip a s $ctl_if | grep inet | grep -v inet6 | sed "s/.*inet//" | cut -f2 -d' ')
+sudo ip address flush $ctl_if
+sudo ovs-vsctl add-port br-ex $ctl_if
+sudo ip a a $ip dev br-ex
+sudo ip link set dev br-ex up
+sudo ip link set dev $ctl_if up
