aed11654625b738b62da217ad614cfaa92e6ccd9
Changjun Wang <310397125@qq.com>
2019-03-05 10:09:26 +0000

    Add Lenovo ThinkCLoud Driver
    add a new branch lenovo
    modify pom.xml under the root
    
    Change-Id: I5edd57bf3e9cc1e32c5a94b9494daf645b220eaa
    Issue-ID: MULTICLOUD-506
    Signed-off-by: Changjun Wang <310397125@qq.com>

---
+++ lenovo/thinkcloud/registration/views/registration.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2017-2018 Lenovo Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from django.conf import settings
+from newton_base.registration import registration as newton_registration
+from common.msapi import extsys
+
+logger = logging.getLogger(__name__)
+
+# DEBUG=True
+
+
+class Registry(newton_registration.Registry):
+
+    def __init__(self):
+        self.proxy_prefix = settings.MULTICLOUD_PREFIX
+        self.aai_base_url = settings.AAI_BASE_URL
+        self._logger = logger
+
+
+class RegistryV1(Registry):
+    def __init__(self):
+        self.proxy_prefix = settings.MULTICLOUD_API_V1_PREFIX
+        self.aai_base_url = settings.AAI_BASE_URL
+        self._logger = logger
+
+    def post(self, request, cloud_owner="", cloud_region_id=""):
+        self._logger.info("registration with : %s, %s" % (cloud_owner, cloud_region_id))
+        self._logger.debug("with data: %s" % request.data)
+
+        vimid = extsys.encode_vim_id(cloud_owner, cloud_region_id)
+        return super(RegistryV1, self).post(request, vimid)
+
+    def delete(self, request, cloud_owner="", cloud_region_id=""):
+        self._logger.debug("unregister cloud region: %s, %s" % (cloud_owner, cloud_region_id))
+
+        vimid = extsys.encode_vim_id(cloud_owner, cloud_region_id)
+        return super(RegistryV1, self).delete(request, vimid)
