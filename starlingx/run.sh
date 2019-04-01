31a6b223d123ff9fb26a6d0fa43ad327440cd5ca
Haibin Huang <haibin.huang@intel.com>
2019-04-01 09:34:30 +0800

    Add https support for multicloud starlingx plugin
    The https endpoint can be enabled by setting env: SSL_ENABLED=true
    
    Change-Id: I2852d705d0d0c11d0b16cb568184296b9c8f25d0
    Issue-ID: MULTICLOUD-535
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- starlingx/run.sh
+++ starlingx/run.sh
@@ -17,6 +17,12 @@ memcached -d -m 2048 -u root -c 1024 -p 11211 -P /tmp/memcached1.pid
 export PYTHONPATH=lib/share
 uwsgi --http :9009 --module starlingx.wsgi --master --processes 4
 
+if [ ${SSL_ENABLED} = "true" ]; then
+    nohup uwsgi --https :9009,starlingx/pub/ssl/cert/cert.crt,starlingx/pub/ssl/cert/cert.key --module starlingx.wsgi --master --processes 4 &
+
+else
+    nohup uwsgi --http :9009 --module starlingx.wsgi --master --processes 4 &
+
 logDir="/var/log/onap/multicloud/openstack/starlingx"
 if [ ! -x  $logDir  ]; then
        mkdir -p $logDir
