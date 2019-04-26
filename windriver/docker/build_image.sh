5a763312cb42881352905d043b806d910062e05e
Bin Yang <bin.yang@windriver.com>
2019-04-26 04:46:55 +0000

    upgrade version to 1.3.1
    1.3.0 was used by integration team for validation
    
    Change-Id: Ib28838cc4d3dca872b7404e572f5db365487cfb9
    Issue-ID: MULTICLOUD-589
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- windriver/docker/build_image.sh
+++ windriver/docker/build_image.sh
@@ -6,8 +6,8 @@ cd ${DOCKER_BUILD_DIR}
 
 BUILD_ARGS="--no-cache"
 ORG="onap"
-VERSION="1.3.0-SNAPSHOT"
-STAGING="1.3.0-STAGING"
+VERSION="1.3.1-SNAPSHOT"
+STAGING="1.3.1-STAGING"
 PROJECT="multicloud"
 IMAGE="openstack-windriver"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
