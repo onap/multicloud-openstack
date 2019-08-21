8b6aad448f8b126f3d832495fa7df7764dd7b39b
Bin Yang <bin.yang@windriver.com>
2019-08-21 08:22:20 +0000

    Fix wsgi error
    Change-Id: I2ce531c85eee63c3306b0a0b40b722f1c5da25c3
    Issue-ID: MULTICLOUD-774
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- windriver/titanium_cloud/wsgi.py
+++ windriver/titanium_cloud/wsgi.py
@@ -19,4 +19,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "titanium_cloud.settings")
 
-APPLICATION = get_wsgi_application()
+application = get_wsgi_application()
