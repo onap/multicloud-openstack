c276df27eec70fe5a6d133e607a9c3cb6a17f3f4
Haibin Huang <haibin.huang@intel.com>
2019-09-03 10:27:42 +0800

    Merge onap and openstack_pike.
    Issue-ID: MULTICLOUD-798
    Signed-off-by: songgongjun <gongjun.song@intel.com>
    Change-Id: I13fe7461c0d9c173970b080cdd2cb73dbae930b0

---
+++ pike/vagrant/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt-get update -y
+sudo apt-get install git -y
+git clone https://github.com/openstack-dev/devstack 
+cd devstack; git checkout stable/pike
+sudo apt-get install openvswitch-switch -y
+sudo ovs-vsctl add-br br-ex
+ip=$(ip a s eth2 | grep inet | grep -v inet6 | sed "s/.*inet//" | cut -f2 -d' ')
+sudo ip address flush eth2
+sudo ovs-vsctl add-port br-ex eth2
+sudo ip a a $ip dev br-ex
+sudo ip link set dev br-ex up
+sudo ip link set dev eth2 up
