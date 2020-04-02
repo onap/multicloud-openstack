44fcaf7a0408fd9f6850f3c1c65e404d05536f22
Bin Yang <bin.yang@windriver.com>
2020-04-02 11:03:28 +0000

    Bump up version to 1.5.6
    Change-Id: Icc7bb5bf48691d0daefc47ba975b3e4fa18d08a4
    Issue-ID: MULTICLOUD-1045
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- windriver/docker/build_image.sh
+++ windriver/docker/build_image.sh
@@ -6,8 +6,8 @@ cd ${DOCKER_BUILD_DIR}
 
 BUILD_ARGS="--no-cache"
 ORG="onap"
-VERSION="1.5.5-SNAPSHOT"
-STAGING="1.5.5-STAGING"
+VERSION="1.5.6-SNAPSHOT"
+STAGING="1.5.6-STAGING"
 PROJECT="multicloud"
 IMAGE="openstack-windriver"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
